24503e1bec6ab0962eab8eb442e63ad0485704a1
Yiran Xu <vivianxu1999@gmail.com>
2020-02-25 16:31:00 -0800

    edit Feb 25

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key='bkEW3qNEGcOMD61eXsvkzxGHW9TdNNjA'
+	var map = L.mapquest.map('map', {
+		center:[32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoonControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
